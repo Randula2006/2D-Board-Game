d85bcc64fb093d6c2272d03d0b95977186144348
Randula Gunathilake <randula30085@gmail.com>
2026-04-16 14:09:51 +0530

    Add structs for Linked list nodes

--- linked_list.h
+++ linked_list.h
@@ -1,5 +1,24 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+typedef struct {
+    int playerRow, playerCol;
+    int enemyRow, enemyCol;
+    int treasureCollected;
+    char enemyFacing;
+    char ** grid;
+    int ** type;
+} GameState;
 
-#endif /*LINKED_LIST_H */
+typedef struct Node{
+    void * data; /* Genaric pointer to store the GameState */
+    struct Node *next;
+    
+}Node;
+
+typedef struct{
+    Node * head;
+    int size;
+} LinkedList;
+
+#endif /* LINKED_LIST_H */
